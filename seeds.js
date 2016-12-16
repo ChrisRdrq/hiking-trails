23f52301ae1cd263a9aa9ff4cf0f023d3035bed7
Chris Roderique <chrisrdrq@gmail.com>
2016-12-16 17:19:22 -0500

    heroku fix?

--- seeds.js
+++ seeds.js
@@ -1,7 +1,22 @@
 var mongoose = require('mongoose');
 var Todo = require('./models/todo');
 
-mongoose.connect('mongodb://localhost/todos');
+
+// Connect to database
+if (process.env.MONGODB_URI) {
+ mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+ mongoose.connect('mongodb://localhost/trails');
+}
+mongoose.connection.on('error', function(err) {
+ console.error('MongoDB connection error: ' + err);
+ process.exit(-1);
+ }
+);
+mongoose.connection.once('open', function() {
+ console.log("Mongoose has connected to MongoDB!");
+});
 
 // our script will not exit until we have disconnected from the db.
 function quit() {
